c354b2236c9941b3c46cf0a8a7d282a69b6d4a66
d3Xt3r <dextersgenius@gmail.com>
2023-11-24 12:03:26 +1300

    Update bazzite-rotation-fix.sh
    Updated comments / typos

--- bazzite-rotation-fix.sh
+++ bazzite-rotation-fix.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# This scripts fixes the screen orientation in the Desktop Mode in Bazzite-Deck KDE
+# This script fixes the screen orientation in the Desktop Mode in Bazzite-Deck KDE
+# Author: d3Xt3r
 # 
 # - Save this script somewhere and mark it as executabe (chmod +x ./bazzite-rotation-fix.sh)
 # - Add it to your KDE Autostart config (Menu > search for Autostart > Add)
@@ -8,6 +9,9 @@
 sleep 1
 echo $(date '+%Y-%m-%d %H:%M:%S') Starting Bazzite Desktop Orientation Fix script...| tee -a /tmp/bazrotfix.log
 
+# This bit is needed to wait allow enough time for the desktop to load, otherwise the fix won't work
+# Since Steam launches automatically in the Desktop mode in Bazzite-Deck, we can use it 
+# to determine whether or not the desktop has loaded.
 while ! pgrep -x "steam" > /dev/null; do
 	echo $(date '+%Y-%m-%d %H:%M:%S') Waiting for Steam to start.. | tee -a /tmp/bazrotfix.log
 	sleep 0.5
@@ -15,11 +19,12 @@ done
 
 sleep 1
 
-# Uncomment below lines if you don't want Steam to run in the background while in Desktop mode
+# Uncomment below lines if you DONT want Steam running in the background while in Desktop mode
 #echo $(date '+%Y-%m-%d %H:%M:%S') Closing Steam... | tee -a /tmp/bazrotfix.log
 #killall -v -r '.*steam.*' 2>&1 | tee -a /tmp/bazrotfix.log
 
-# rotation options: right, normal, left, inverted
+# Fix desktop orientation
+# Rotation options: right, normal, left, inverted
 echo $(date '+%Y-%m-%d %H:%M:%S') Fixing desktop orientation... | tee -a /tmp/bazrotfix.log
 kscreen-doctor output.1.rotation.normal 2>&1 | tee -a /tmp/bazrotfix.log
 
